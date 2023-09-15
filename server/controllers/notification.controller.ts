c6b5d7f6639e153acb6816322ec3c8d6f36ffa85
Francisco Oro <francisco.oro@comunidad.unam.mx>
2023-09-15 14:08:53 -0600

    notifications

---
+++ server/controllers/notification.controller.ts
@@ -0,0 +1,46 @@
+import NotificationModel from "../models/notification.model";
+import { NextFunction, Request, Response } from "express";
+import { CatchAsyncError } from "../middleware/catchAsyncErrors";
+import ErrorHandler from "../utils/ErrorHandler";
+
+// Get all notifications -- only for admin
+export const getNotifications = CatchAsyncError(async (req:Request, res:Response, next: NextFunction) => {
+    try {
+        const notifications = await NotificationModel.find().sort({createdAt: -1});
+
+        res.status(200).json({
+            success: true,
+            notifications
+        }); 
+
+    } catch (error:any) {
+        return next(new ErrorHandler(error.message, 500));
+    }
+});
+
+// Update notification status
+
+export const updateNotification = CatchAsyncError( async (req:Request, res:Response, next:NextFunction) => {
+    try {
+        const notification = await NotificationModel.findById(req.params.id);
+        if (!notification) {
+            return next(new ErrorHandler("Notification not found", 404));
+        } else {
+            notification.status ? notification.status = 'read' : notification?.status;
+        }
+
+        await notification.save();
+
+        const notifications = await NotificationModel.find().sort({
+            createdAt: -1,
+        });
+
+        res.status(201).json({
+            success:true,
+            notifications
+        }); 
+
+    } catch (error:any) {
+        return next(new ErrorHandler(error.message, 500))
+    }
+})
