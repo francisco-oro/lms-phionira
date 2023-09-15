bd87e96946c7181cf881d3988ac0212c60e82a71
Francisco Oro <francisco.oro@comunidad.unam.mx>
2023-09-15 13:46:13 -0600

    create-order

---
+++ server/controllers/order.controller.ts
@@ -0,0 +1,90 @@
+import { NextFunction, Request, Response } from "express";
+import { CatchAsyncError } from "../middleware/catchAsyncErrors";
+import ErrorHandler from "../utils/ErrorHandler";
+import OrderModel, {IOrder}from "../models/order.model";
+import userModel from "../models/user.model";
+import CourseModel from "../models/course.model";
+import path from "path";
+import ejs from "ejs"; 
+import sendMail from "../utils/sendMail";
+import NotificationModel from "../models/notification.model";
+import { newOrder } from "../services/order.service";
+
+// Create order
+export const createOrder = CatchAsyncError(async(req:Request, res:Response, next:NextFunction) => {
+    try {
+        const {courseId, payment_info, qty } = req.body as IOrder;
+
+        const user = await userModel.findById(req.user?._id);
+
+        const courseExistInUser = await user?.courses.some((course:any) => course._id.toString() === courseId); 
+
+        if (courseExistInUser) {
+            return next(new ErrorHandler("Ya has comprado este curso", 400));
+
+        }
+
+        const course = await CourseModel.findById(courseId);
+
+        if (!course) {
+            return next(new ErrorHandler("Course not found", 404));
+
+        }
+
+        const data:any = {
+            courseId: course._id,
+            userId: user?._id,
+            payment_info, 
+            qty
+        };
+
+        console.log(data);
+
+        const mailData = {
+            order: {
+                _id: course._id.toString().slice(0,6),
+                name: course.name, 
+                price: course.price,
+                date: new Date().toLocaleDateString('es', {year:'numeric', month: 'long', day:'numeric'}),
+                qty: qty,
+                username: user?.name,
+                email: user?.email,
+            }
+        }
+
+        console.log(mailData.order);
+
+        const html = await ejs.renderFile(path.join(__dirname, '../mails/order-confirmation.ejs'), {order: mailData});
+        
+        try {
+            if (user) {
+                await sendMail({
+                    email: user.email,
+                    subject: "Confirimación de Orden | Phionira",
+                    template: "order-confirmation.ejs",
+                    data: mailData
+                }); 
+            }
+        } catch (error:any) {
+            return next(new ErrorHandler(error.message, 500)); 
+        }
+
+        user?.courses.push(course?._id);
+        await user?.save(); 
+
+        await NotificationModel.create({
+            user: user?._id,
+            title:  `Nueva orden`,
+            message: `Hay una nueva compra en ${course?.name}`
+        });
+
+        course.purchased ? course.purchased += 1 : course.purchased; 
+
+        await course.save();
+
+        newOrder(data, res, next);
+
+    } catch (error:any) {
+        return next(new ErrorHandler(error.message, 500));
+    }
+})
--- server/models/order.model.ts
+++ server/models/order.model.ts
@@ -1,26 +1,33 @@
-import mongoose, {Document, Model, Schema} from "mongoose";
-export interface IOrder extends Document {
-    courseId: string; 
-    userId: string;
+import mongoose, {Document,Model,Schema} from "mongoose";
+
+
+export interface IOrder extends Document{
+    courseId: string;
+    userId?:string;
     payment_info: object;
+    qty: number,
 }
 
-const OrderSchema = new Schema<IOrder>({
+const orderSchema = new Schema<IOrder>({
     courseId: {
-        type: String, 
+     type: String,
+     required: true
+    },
+    userId:{
+        type: String,
         required: true
     },
-    userId: {
-        type: String, 
-        required: true, 
-    }, 
-    payment_info: {
-        type:Object,
-        // required: true,
+    payment_info:{
+        type: Object,
+        // required: true
     },
+    qty: {
+        type: Number, 
+        required: true,
+        default: 1, 
+    }
+},{timestamps: true});
 
-}, {timestamps: true});
-
-const OrderModel = Model<IOrder> = mongoose.model("Order", OrderSchema);
+const OrderModel: Model<IOrder> = mongoose.model('Order',orderSchema);
 
 export default OrderModel;
